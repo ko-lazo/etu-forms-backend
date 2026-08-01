cf124084ecb3825925208229c86571fa4c04dc45
infant-annihilator <konstantin.lazo@gmail.com>
2026-08-01 19:13:59 +0300

    feat(api-token): add routes

---
+++ src/modules/api-token/api-token.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { ApiTokenController } from "./api-token.controller.js";
+import { validate } from "@/shared/http/middleware/validate.middleware.js";
+import { apiTokenDto } from "@/modules/api-token/api-token.dto.js";
+import { AuthMiddleware } from "@/shared/http/middleware/auth.middleware.js";
+
+export function createApiTokenRoutes(
+  controller: ApiTokenController,
+  authMiddleware: AuthMiddleware,
+): Router {
+  const router = Router();
+
+  router.use(authMiddleware.handle);
+
+  router.get("/", controller.findAll);
+  router.delete("/:id", controller.delete);
+  router.post("/", validate(apiTokenDto.createSchema), controller.create);
+
+  return router;
+}
