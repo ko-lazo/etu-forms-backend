01e4060094e8e0274bb5bd89b1fa0a69993f1f42
infant-annihilator <konstantin.lazo@gmail.com>
2026-08-01 19:11:15 +0300

    feat(api-token): add controller

---
+++ src/modules/api-token/api-token.controller.ts
@@ -0,0 +1,39 @@
+import type { Request, Response } from "express";
+import { BaseController } from "@/core/controllers/base.controller.js";
+import { ApiToken } from "@/modules/api-token/api-token.types.js";
+import {
+  ApiTokenResponseDto,
+  CreateApiTokenDto,
+  UpdateApiTokenDto,
+} from "@/modules/api-token/api-token.dto.js";
+import { ApiTokenService } from "@/modules/api-token/api-token.service.js";
+import { ApiTokenGeneratorService } from "@/modules/api-token/api-token-generator.service.js";
+import { NotFoundError } from "@/shared/errors/not-found.error.js";
+
+export class ApiTokenController extends BaseController<
+  ApiToken,
+  CreateApiTokenDto,
+  UpdateApiTokenDto,
+  ApiTokenResponseDto
+> {
+  constructor(
+    service: ApiTokenService,
+    private readonly generatorService: ApiTokenGeneratorService,
+  ) {
+    super(service);
+  }
+
+  override create = async (req: Request, res: Response): Promise<void> => {
+    const userId = req.user?.id;
+    const dto: CreateApiTokenDto = req.body;
+
+    if (!userId) {
+      throw new NotFoundError("...");
+    }
+
+    const result = await this.generatorService.generate(userId, dto);
+
+    const data = this.mapper ? this.mapper.toResponse(result) : result;
+    res.status(201).json(data);
+  };
+}
