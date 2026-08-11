68e45cf2e29a4b55bc8f007af02c596b788a1f2f
infant-annihilator <konstantin.lazo@gmail.com>
2026-08-11 10:55:31 +0300

    feat(form): implement public view logic based on publication dates

--- src/modules/form/form.policy.ts
+++ src/modules/form/form.policy.ts
@@ -2,8 +2,14 @@ import { IResourcePolicy } from "@/core/policies/policy.interface.js";
 import { Form } from "./form.types.js";
 
 export class FormPolicy implements IResourcePolicy<Form> {
-  view(userId: string, form: Form): boolean {
-    return form.userId === userId;
+  view(userId: string | undefined, form: Form): boolean {
+    const now = new Date();
+    const isPublished = form.publishedAt && new Date(form.publishedAt) <= now;
+    const isNotArchived = !form.archivedAt || new Date(form.archivedAt) > now;
+    if (isPublished && isNotArchived) {
+      return true;
+    }
+    return !!userId && form.userId === userId;
   }
 
   update(userId: string, form: Form): boolean {
