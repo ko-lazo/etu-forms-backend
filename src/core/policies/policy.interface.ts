e1a0c3bcc82c25f265cf3d6056c48f496e4f4dab
infant-annihilator <konstantin.lazo@gmail.com>
2026-08-17 16:31:02 +0300

    refactor(policies): replace old policy interface with stricter new one

--- src/core/policies/policy.interface.ts
+++ src/core/policies/policy.interface.ts
@@ -1,6 +1,6 @@
 type Awaitable<T> = T | Promise<T>;
 
-export type ResourcePolicy<TEntity, TCreateContext = void> = {
+export type IResourcePolicy<TEntity, TCreateContext = void> = {
   readonly view: (
     userId: string | undefined,
     entity: TEntity,
@@ -21,23 +21,3 @@ export type ResourcePolicy<TEntity, TCreateContext = void> = {
     entity: TEntity,
   ) => Awaitable<boolean>;
 };
-
-export interface IResourcePolicy<TEntity> {
-  view(userId: string | undefined, entity: TEntity): boolean | Promise<boolean>;
-
-  update(userId: string, entity: TEntity): boolean | Promise<boolean>;
-
-  delete(userId: string, entity: TEntity): boolean | Promise<boolean>;
-}
-
-export interface ISubResourcePolicy<TEntity> extends IResourcePolicy<TEntity> {
-  create?(
-    userId: string | undefined,
-    parentId: string,
-  ): boolean | Promise<boolean>;
-
-  update(
-    userId: string | undefined,
-    entity: TEntity,
-  ): boolean | Promise<boolean>;
-}
