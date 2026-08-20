e324cc246a39e9b326f8e71516e0a74a9aa2468d
infant-annihilator <konstantin.lazo@gmail.com>
2026-08-20 10:59:02 +0300

    feat(form): export formElementNameSchema

--- src/modules/form/index.ts
+++ src/modules/form/index.ts
@@ -1,7 +1,10 @@
 export type { Form } from "./form.types.js";
 export { isOwnedBy } from "./form.domain.js";
 export type { FormSchema } from "./schema/form-schema.schema.js";
-export { formSchemaObject } from "./schema/form-schema.schema.js";
+export {
+  formSchemaObject,
+  formElementNameSchema,
+} from "./schema/form-schema.schema.js";
 
 export type { FormService } from "./form.service.js";
 export type { FormPolicy } from "./form.policy.js";
